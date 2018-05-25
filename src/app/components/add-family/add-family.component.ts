d287591892890ace60b6efcfb89fa792e98b3e6b
Jacob Lewis <jacob.lewis@sony.com>
2018-05-25 16:44:45 +0100

    can add families!

--- src/app/components/add-family/add-family.component.ts
+++ src/app/components/add-family/add-family.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
+
 import { FlashMessagesService } from 'angular2-flash-messages';
 import { FamilyService } from '../../services/family.service';
-
-import { Family } from '../../models/Family';
 
 @Component({
   selector: 'app-add-family',
@@ -11,72 +11,81 @@ import { Family } from '../../models/Family';
 })
 export class AddFamilyComponent implements OnInit {
 
-  membersList = [0,1,2,3,4,5,6,7,8,9];
-
-  family: Family = {
-    name: '',
-    password: '',
-    address: '',
-    day: true,
-    photo: '',
-    members: [
-      {
-        name: ''
-      },
-      {
-        name: ''
-      },
-      {
-        name: ''
-      },
-      {
-        name: ''
-      },
-      {
-        name: ''
-      },
-      {
-        name: ''
-      },
-      {
-        name: ''
-      },
-      {
-        name: ''
-      },
-      {
-        name: ''
-      },
-      {
-        name: ''
-      }
-    ]
-  }
- 
+  familyForm: FormGroup;
 
   constructor(
     private flashMessage: FlashMessagesService,
-    private familyService: FamilyService
+    private familyService: FamilyService,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit() {
+    this.familyForm = this.fb.group({
+      name: '',
+      password: '',
+      address: '',
+      photo: '',
+      day: '',
+      members: this.fb.array([])
+    })
   }
 
-  onSubmit({value, valid}: {value: Family, valid: boolean}) {
-    console.log(value);
-    if(!valid) {
-      // Show error
+  get memberForms() {
+    return this.familyForm.get('members') as FormArray
+  }
+
+  addMember() {
+
+    const member = this.fb.group({
+      firstName: '',
+      lastName: '',
+      rsvp: '',
+      dietry: '',
+      rsvpJake: '',
+      song: ''
+    })
+
+    this.memberForms.push(member);
+  }
+
+  deleteMember(i) {
+    this.memberForms.removeAt(i)
+  }
+
+  async submitHandler() {
+    const formValue = this.familyForm.value;
+    const formValid = this.familyForm.valid;
+
+    if (!formValid) {
       this.flashMessage.show('Please fill out the form correctly', {
         cssClass: 'message is-warning', timeout: 4000
       });
     } else {
-      // Add new family
-      this.familyService.newFamily(value);
-      // Show message
-      this.flashMessage.show('New family added', {
-        cssClass: 'message is-success', timeout: 4000
+      // Checking if password is the same as another
+      this.familyService.getFamiliesByPassword(formValue.password).subscribe(family => {
+        if (family.length > 0) {
+          this.flashMessage.show('Family password already used, use another', {
+            cssClass: 'message is-danger', timeout: 4000
+          });
+        } else {
+          try {
+            this.familyService.newFamily(formValue);
+            console.log('Family added: ', formValue);
+            // Show message
+            this.flashMessage.show('New family added', {
+              cssClass: 'message is-success', timeout: 4000
+            });
+          } catch (err) {
+            // Show error
+            this.flashMessage.show(err, {
+              cssClass: 'message is-danger', timeout: 4000
+            });
+            console.error(err);
+          }
+        }
       });
     }
+
   }
 
 }
